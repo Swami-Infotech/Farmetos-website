8daa600dbe5ccd6e08754cda40c49dd7ce1378d2
Smit Bhatt <smit@swamiinfotech.in>
2025-04-16 18:25:15 +0530

    Changes in Pagination

--- src/app/Component/ProductList/product-list/product-list.component.ts
+++ src/app/Component/ProductList/product-list/product-list.component.ts
@@ -9,25 +9,25 @@ import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-product-list',
-  imports: [HeaderComponent,FooterComponent,NgxPaginationModule,FormsModule,CommonModule],
+  imports: [HeaderComponent, FooterComponent, NgxPaginationModule, FormsModule, CommonModule],
   templateUrl: './product-list.component.html',
   styleUrl: './product-list.component.css'
 })
 export class ProductListComponent implements OnInit {
 
   products: any[] = [];
-  itemsPerPage: number = 12;  
-  currentPage: number = 1;
+  itemsPerPage: number = 12;
+  currentPage: number = 0;
   totalItems: number = 0;
   totalPages: number = 0;
   categoryID!: number;
   categoryName: string = '';
 
   ngOnInit(): void {
-    this.currentPage = 1;
+    this.currentPage = 0;
 
     this.route.queryParams.subscribe(params => {
-      this.categoryName = params['name']; 
+      this.categoryName = params['name'];
     });
     this.route.paramMap.subscribe(params => {
       this.categoryID = Number(params.get('categoryID'));
@@ -37,41 +37,39 @@ export class ProductListComponent implements OnInit {
     });
   }
 
-  constructor(private service:WebService,private route: ActivatedRoute,
-    private router: Router,){}
+  constructor(private service: WebService, private route: ActivatedRoute,
+    private router: Router,) { }
 
 
-    getproduct(CategoryID: number, pageNumber: number) {
-      this.service.getproductbycategory(CategoryID, pageNumber).subscribe(
-        (resp: any) => {
-          if (resp.status) {
-            this.products = resp.data.products;
-            this.totalItems = resp.data.totalCount;
-            this.totalPages = Math.ceil(this.totalItems / this.itemsPerPage);
-          }
-        },
-        (error) => {
-          console.error("Error fetching products:", error);
+  getproduct(CategoryID: number, pageNumber: number) {
+    this.service.getproductbycategory(CategoryID, pageNumber).subscribe(
+      (resp: any) => {
+        if (resp.status) {
+          this.products = resp.data.products;
+          this.totalItems = resp.data.totalCount;
+          this.totalPages = Math.ceil(this.totalItems / this.itemsPerPage);
         }
-      );
-    }
-
-    getPages(): number[] {
-      return Array(this.totalPages).fill(0).map((_, i) => i + 1);
-    }
-    
-    goToPage(page: number) {
-      if (page !== this.currentPage) {
-        this.currentPage = page;
-        this.getproduct(this.categoryID, this.currentPage);
-        this.scrollToTop();
+      },
+      (error) => {
+        console.error("Error fetching products:", error);
       }
+    );
+  }
+
+  getPages(): number[] {
+    return Array(this.totalPages).fill(0).map((_, i) => i + 1);
+  }
+
+  goToPage(page: number) {
+    if (page !== this.currentPage + 1) {
+      this.currentPage = page - 1;
+      this.getproduct(this.categoryID, this.currentPage);
+      this.scrollToTop();
     }
-
-
+  }
 
   nextPage() {
-    if (this.currentPage < this.totalPages) {
+    if (this.currentPage + 1 < this.totalPages) {
       this.currentPage++;
       this.getproduct(this.categoryID, this.currentPage);
       this.scrollToTop();
@@ -79,7 +77,7 @@ export class ProductListComponent implements OnInit {
   }
 
   prevPage() {
-    if (this.currentPage > 1) {
+    if (this.currentPage > 0) {
       this.currentPage--;
       this.getproduct(this.categoryID, this.currentPage);
       this.scrollToTop();
